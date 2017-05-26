f71643e7a83dd67350ff2b8163c074296a70e2e9
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-05-26 12:36:28 -0400

    0.2.0

---
+++ dist/cjs/index.js
@@ -0,0 +1,78 @@
+/*can-search-select@0.1.0#index*/
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.ViewModel = undefined;
+var _canComponent = require('can-component');
+var _canComponent2 = _interopRequireDefault(_canComponent);
+var _map = require('can-define/map/map');
+var _map2 = _interopRequireDefault(_map);
+require('./styles.less.css');
+var _template = require('./template.stache.js');
+var _template2 = _interopRequireDefault(_template);
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { default: obj };
+}
+var ViewModel = exports.ViewModel = _map2.default.extend({
+    filterPropName: 'string',
+    format: function format(item) {
+        return this.filterPropName && item[this.filterPropName] || item;
+    },
+    placeholderSearch: { value: 'Enter to Search...' },
+    placeholderSelect: { value: 'Choose From the List' },
+    btnClass: { value: 'caret' },
+    selectedItem: { type: '*' },
+    items: { type: '*' },
+    val: { type: 'string' },
+    isOpened: 'boolean',
+    searchString: { type: 'string' },
+    itemsFiltered: {
+        get: function get() {
+            return this.searchString ? this.items && this.items.filter(makeFilter(this.searchString, this.filterPropName)) : this.items;
+        }
+    },
+    placeholder: {
+        get: function get(val) {
+            return val || this.placeholderSearch;
+        }
+    },
+    open: function open() {
+        if (!this.searchString && this.isOpened) {
+            this.isOpened = false;
+            this.placeholder = this.placeholderSearch;
+            return;
+        }
+        this.val = '';
+        this.searchString = '';
+        this.isOpened = true;
+        this.placeholder = this.placeholderSelect;
+    },
+    onKeyUp: function onKeyUp(val) {
+        this.val = val;
+        this.searchString = val;
+        this.isOpened = true;
+    },
+    onFocus: function onFocus() {
+        this.placeholder = this.placeholderSearch;
+    },
+    onEnter: function onEnter() {
+        if (this.itemsFiltered.length) {
+            this.select(this.itemsFiltered[0]);
+        }
+    },
+    select: function select(item) {
+        this.selectedItem = item;
+        this.val = this.format(item);
+        this.isOpened = false;
+    }
+});
+function makeFilter(needle, propName) {
+    return function (item) {
+        var stack = propName ? item[propName] : item;
+        return stack.toLowerCase().search(needle.toLowerCase()) !== -1;
+    };
+}
+exports.default = _canComponent2.default.extend({
+    tag: 'can-search-select',
+    ViewModel: ViewModel,
+    view: _template2.default
+});
