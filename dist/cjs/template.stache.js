06f79f690bb4bcb5e69fa788ceaba211c22f652c
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-05-30 12:30:25 -0400

    0.2.4

---
+++ dist/cjs/template.stache.js
@@ -0,0 +1,418 @@
+/*can-search-select@0.2.3#template.stache!steal-stache@3.0.7#steal-stache*/
+var stache = require('can-stache');
+var mustacheCore = require('can-stache/src/mustache_core');
+require('can-view-import@3.0.7#can-view-import');
+require('can-stache-bindings@3.1.5#can-stache-bindings');
+var renderer = stache([
+    {
+        'tokenType': 'start',
+        'args': [
+            'div',
+            false
+        ]
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['input-group']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'end',
+        'args': [
+            'div',
+            false
+        ]
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n  ']
+    },
+    {
+        'tokenType': 'start',
+        'args': [
+            'input',
+            true
+        ]
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['type']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['text']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['type']
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['form-control']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['placeholder']
+    },
+    {
+        'tokenType': 'special',
+        'args': ['placeholder']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['placeholder']
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['($focus)']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['onFocus()']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['($focus)']
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['($keyup)']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['onKeyUp(%element.value)']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['($keyup)']
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['($enter)']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['onEnter(%element.value)']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['($enter)']
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['{$value}']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['val']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['{$value}']
+    },
+    {
+        'tokenType': 'end',
+        'args': [
+            'input',
+            true
+        ]
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n  ']
+    },
+    {
+        'tokenType': 'start',
+        'args': [
+            'span',
+            false
+        ]
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['input-group-btn']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'end',
+        'args': [
+            'span',
+            false
+        ]
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n    ']
+    },
+    {
+        'tokenType': 'start',
+        'args': [
+            'button',
+            false
+        ]
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['btn btn-default']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['type']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['button']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['type']
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['($click)']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['open()']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['($click)']
+    },
+    {
+        'tokenType': 'end',
+        'args': [
+            'button',
+            false
+        ]
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n      ']
+    },
+    {
+        'tokenType': 'start',
+        'args': [
+            'span',
+            false
+        ]
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'special',
+        'args': ['btnClass']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'end',
+        'args': [
+            'span',
+            false
+        ]
+    },
+    {
+        'tokenType': 'close',
+        'args': ['span']
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n    ']
+    },
+    {
+        'tokenType': 'close',
+        'args': ['button']
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n  ']
+    },
+    {
+        'tokenType': 'close',
+        'args': ['span']
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n']
+    },
+    {
+        'tokenType': 'close',
+        'args': ['div']
+    },
+    {
+        'tokenType': 'special',
+        'args': ['#if isOpened']
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n  ']
+    },
+    {
+        'tokenType': 'start',
+        'args': [
+            'ul',
+            false
+        ]
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['dropdown-list']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['class']
+    },
+    {
+        'tokenType': 'end',
+        'args': [
+            'ul',
+            false
+        ]
+    },
+    {
+        'tokenType': 'special',
+        'args': ['#each itemsFiltered as item']
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n      ']
+    },
+    {
+        'tokenType': 'start',
+        'args': [
+            'li',
+            false
+        ]
+    },
+    {
+        'tokenType': 'attrStart',
+        'args': ['($click)']
+    },
+    {
+        'tokenType': 'attrValue',
+        'args': ['select(item)']
+    },
+    {
+        'tokenType': 'attrEnd',
+        'args': ['($click)']
+    },
+    {
+        'tokenType': 'end',
+        'args': [
+            'li',
+            false
+        ]
+    },
+    {
+        'tokenType': 'special',
+        'args': ['format(item)']
+    },
+    {
+        'tokenType': 'close',
+        'args': ['li']
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['    ']
+    },
+    {
+        'tokenType': 'special',
+        'args': ['else']
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n      ']
+    },
+    {
+        'tokenType': 'start',
+        'args': [
+            'li',
+            false
+        ]
+    },
+    {
+        'tokenType': 'end',
+        'args': [
+            'li',
+            false
+        ]
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['No records found']
+    },
+    {
+        'tokenType': 'close',
+        'args': ['li']
+    },
+    {
+        'tokenType': 'special',
+        'args': ['/each']
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n  ']
+    },
+    {
+        'tokenType': 'close',
+        'args': ['ul']
+    },
+    {
+        'tokenType': 'special',
+        'args': ['/if']
+    },
+    {
+        'tokenType': 'chars',
+        'args': ['\n']
+    },
+    {
+        'tokenType': 'done',
+        'args': []
+    }
+]);
+module.exports = function (scope, options, nodeList) {
+    var moduleOptions = { module: module };
+    if (!(options instanceof mustacheCore.Options)) {
+        options = new mustacheCore.Options(options || {});
+    }
+    return renderer(scope, options.add(moduleOptions), nodeList);
+};
